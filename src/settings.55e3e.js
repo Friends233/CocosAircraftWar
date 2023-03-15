9d811879234b8ba4b252f7a1c1355c2721799f44
Friends233 <Friends233@foxmail.com>
2023-03-15 17:39:58 +0800

    重新打包

--- src/settings.55e3e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"e5e0d",main:"cb762"}};
